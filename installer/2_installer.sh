e867ae0ad31ca0f48ac0d5c1b86a71ba63433bf1
seonghoonko <53179799+seonghoonko@users.noreply.github.com>
2024-04-29 18:29:52 +0900

    Create 2_installer.sh
    add 2_installer.sh

---
+++ installer/2_installer.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# build openpilot & install plotjuggler
+scons -u -j$(nproc) ; 
+cd ~/openpilot/tools/plotjuggler ; 
+./juggle.py --install
+
+#add .bashrc for qt (network connection)
+cd ~;
+sed '98 i\
+#Custom alias
+alias op_plot='cd ~/openpilot/tools/plotjuggler&&./juggle.py --stream’' .bashrc;
+sed '$s/$/\n
+export PYTHONPATH="/home/$USER/openpilot/.venv/bin/python3:/home/$USER
+/openpilot"
+export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0"
+export LIBGL_ALWAYS_INDIRECT=1
+export DISPLAY=$WSL_IF_IP:0
+unset LIBGL_ALWAYS_INDIRECT
+/g' .bashrc;
