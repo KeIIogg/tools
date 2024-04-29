df964d8ca67a494317eab66dc7a479b63ace8581
koseonghoon <68089983+koseonghoon@users.noreply.github.com>
2024-04-29 19:24:04 +0900

    Update 2_installer.sh

--- installer/2_installer.sh
+++ installer/2_installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # build openpilot & install plotjuggler
-scons -u -j$(nproc) ; 
+cd ~/openpilot && scons -u -j$(nproc) ; 
 cd ~/openpilot/tools/plotjuggler ; 
 ./juggle.py --install
 
