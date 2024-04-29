43ce3dc818518aa54faf1ebc3c2731a5fcc4c215
koseonghoon <68089983+koseonghoon@users.noreply.github.com>
2024-04-29 22:43:14 +0900

    Update 2_installer.sh

--- installer/2_installer.sh
+++ installer/2_installer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 #add .bashrc for qt (network connection)
-sed -i'' -r -e "$You may want to put all your additions into a separate file like$i\alias op_plot='cd ~/openpilot/tools/plotjuggler&&./juggle.py --stream'$" ~/.bashrc;
+sed -i'' -r -e "/You may want to put all your additions into a separate file like/i\alias op_plot='cd ~/openpilot/tools/plotjuggler&&./juggle.py --stream'/" ~/.bashrc;
