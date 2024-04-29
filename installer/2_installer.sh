cf28cca2ac5899fbf8fefc2696dee640767248f1
koseonghoon <68089983+koseonghoon@users.noreply.github.com>
2024-04-29 21:44:46 +0900

    Update 2_installer.sh

--- installer/2_installer.sh
+++ installer/2_installer.sh
@@ -6,15 +6,5 @@ sudo su - root;
 chmod +x bash.bashrc;
 chmod +w bash.bashrc;
 
-sed '98 i\
-alias op_plot='cd ~/openpilot/tools/plotjuggler&&./juggle.py --stream'' bash.bashrsc;
 
-sed '$s/$/\n
-export PYTHONPATH="/home/$USER/openpilot/.venv/bin/python3:/home/$USER
-/openpilot"
-export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0"
-export LIBGL_ALWAYS_INDIRECT=1
-export DISPLAY=$WSL_IF_IP:0
-unset LIBGL_ALWAYS_INDIRECT
-/g' bash.bashrsc;
 
