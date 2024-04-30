c53cd8c63d7fdb983b48a24c50890fae5ae193e1
koseonghoon <68089983+koseonghoon@users.noreply.github.com>
2024-04-30 09:37:17 +0900

    Update and rename 1_installer.sh to installer.sh

--- installer/installer.sh
+++ installer/installer.sh
@@ -19,8 +19,7 @@ sudo apt install git-lfs;
 cd ~ ; 
 git clone --recurse-submodules https://github.com/commaai/openpilot.git ;
 cd ~/openpilot ; git lfs pull ; 
-export PYTHONPATH="/home/$USER/openpilot/.venv/bin/python3:/home/$USER
-/openpilot";
+export PYTHONPATH="/home/$USER/openpilot/.venv/bin/python3:/home/$USER/openpilot";
 export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0";
 export LIBGL_ALWAYS_INDIRECT=1;
 export DISPLAY=$WSL_IF_IP:0;
