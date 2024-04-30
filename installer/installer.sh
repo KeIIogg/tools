a2d04facacf24e17f500e9d5d78bd43180d2689f
koseonghoon <68089983+koseonghoon@users.noreply.github.com>
2024-04-30 09:45:33 +0900

    Update installer.sh

--- installer/installer.sh
+++ installer/installer.sh
@@ -32,5 +32,9 @@ cd ~/openpilot;
 scons -u -j$(nproc) ; 
 
 # install plotjuggler
+export PYTHONPATH="/home/$USER/openpilot/.venv/bin/python3:/home/$USER/openpilot";
+export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0";
+export LIBGL_ALWAYS_INDIRECT=1;
+export DISPLAY=$WSL_IF_IP:0;
 cd ~/openpilot/tools/plotjuggler ; 
 ./juggle.py --install ;
