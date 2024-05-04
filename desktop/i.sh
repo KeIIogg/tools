cd3a067603b34f61966c8ee960610b5bee7b884d
seonghoonko <53179799+seonghoonko@users.noreply.github.com>
2024-05-04 23:50:03 +0900

    Update i.sh

--- desktop/i.sh
+++ desktop/i.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 cd ~; 
-git clone -n --depth=1 --filter=tree:0 https://github.com/seonghoonko/tools.git && cd ~/tools && git sparse-checkout set --no-cone installer && git checkout;
-cd ~/tools/Plot_installer;
+git clone -n --depth=1 --filter=tree:0 https://github.com/seonghoonko/tools.git && cd ~/tools && git sparse-checkout set --no-cone plot_installer && git checkout;
+cd ~/tools/plot_installer;
 chmod +x installer.sh; 
 ./installer.sh;
