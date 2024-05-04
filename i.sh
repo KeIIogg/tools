1e0659551df9eef48d8afb2a993889d7ee7eb72f
seonghoonko <53179799+seonghoonko@users.noreply.github.com>
2024-05-04 23:37:44 +0900

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 cd ~; 
-git clone -n --depth=1 --filter=tree:0 https://github.com/seonghoonko/plotjuggler.git && cd ~/plotjuggler && git sparse-checkout set --no-cone installer && git checkout;
-cd ~/plotjuggler/installer;
+git clone -n --depth=1 --filter=tree:0 https://github.com/seonghoonko/tools.git && cd ~/plotjuggler && git sparse-checkout set --no-cone installer && git checkout;
+cd ~/tools/plot_installer;
 chmod +x installer.sh; 
 ./installer.sh;
