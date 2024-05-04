45fe31ce6d2821ee58b4a3d9e233d6e7b8260c13
koseonghoon <68089983+koseonghoon@users.noreply.github.com>
2024-05-04 17:33:06 +0900

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/usr/bin/bash
 cd ~; 
 git clone -n --depth=1 --filter=tree:0 https://github.com/seonghoonko/plotjuggler.git && cd ~/plotjuggler && git sparse-checkout set --no-cone installer && git checkout;
 cd ~/plotjuggler/installer;
